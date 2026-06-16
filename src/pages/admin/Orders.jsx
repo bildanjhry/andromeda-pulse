f4cb66dcbb27b4106359d7ac79d1eff80c9a7414
bildan <bildanjauhary@gmail.com>
2026-06-17 00:03:41 +0700

    feat: add admin orders page with table

--- src/pages/admin/Orders.jsx
+++ src/pages/admin/Orders.jsx
@@ -1,9 +1,115 @@
+// component
 import DashboardLayout from "@/components/layouts/DashboardLayout"
+
+// asset
+import Export from "@/assets/icons/export-white.svg"
+import Search from "@/assets/icons/search-mute.svg"
+import Watch from "@/assets/icons/watch-mute.svg"
 
 export default function Orders(){
   return(
     <DashboardLayout>
-            
+      <div className="px-6 mb-10">
+        <header className="flex flex-col gap-5 mt-6">
+          <div className="flex justify-between h-10 items-center">
+            <div className="flex flex-col justify-center gap-0">
+              <p className="text-h text-2xl font-medium">Manajemen Pesanan</p>
+            </div>
+            <button className="w-[100px] h-full bg-(--main-bg) flex justify-center items-center
+            text-white text-sm rounded-xl gap-2">
+              <img src={Export} alt="add product" />
+              <p>Export</p>
+            </button>
+          </div> 
+          <div className="flex mt-4 items-center gap-2 h-[38px]">
+            <button className="w-[102px] bg-(--main-bg) text-white flex justify-center items-center
+						rounded-xl h-full text-sm">Semua (8)</button>
+            <button className="w-[102px] border-light flex justify-center items-center
+						rounded-xl h-full text-sm bg-white">Pending (1)</button>
+            <button className="w-[102px] border-light flex justify-center items-center
+						rounded-xl h-full text-sm bg-white">Dikemas (1)</button>
+            <button className="w-[102px] border-light flex justify-center items-center
+						rounded-xl h-full text-sm bg-white">Dikirim (2)</button>
+            <button className="w-[102px] border-light flex justify-center items-center
+						rounded-xl h-full text-sm bg-white">Terkirim (3)</button>
+          </div>
+          <form 
+            className="bg-white border-light w-full justify-between rounded-lg p-4 
+						gap-0 grid grid-cols-[88%_11%] h-19"
+            action="">
+            <div className="relative h-full w-full text-sm">
+              <img 
+                className="absolute top-3 left-4"
+                src={Search} alt="search" />
+              <input
+                placeholder="Cari produk atau merk"
+                className="h-full rounded-xl w-full bg-(--input-bg) border-light pl-10"
+                type="search" name="search" id="" />
+            </div>
+            <button className="h-full rounded-xl border-light">
+              <p className="text-sm">Filter</p>
+            </button>
+          </form>
+          <div className="mt-2 rounded-xl overflow-hidden border-light">
+            <table className="rounded-xl">
+              <thead>
+                <tr className="h-12 text-sm bg-(--input-bg) text-left">
+                  <th className="pl-4">No. Pesanan</th>
+                  <th>Pelanggan</th>
+                  <th>Tanggal</th>
+                  <th>Item</th>
+                  <th>Total</th>
+                  <th>Pembayaran</th>
+                  <th>Status</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="h-13 bg-white text-sm">
+                  <td className="pl-4 text-(--text-high) w-47 text-sm font-semibold">
+                    <p>#67263KOU</p>
+                  </td>
+                  <td className="flex flex-col justify-end pt-1 text-sm w-60	">
+                    <p className="text-h">Budi</p>
+                    <p>budi@mail.com</p>
+                  </td>
+                  <td className="text-xs w-33">
+                    <p>28 Mei 2026</p>
+                  </td>
+                  <td className="w-22">
+                    <p>1</p>
+                  </td>
+                  <td className="text-(--text-high) w-44">
+                    <p>Rp 400.000</p>
+                  </td>
+                  <td className="w-40">
+                    <p>Gopay</p>
+                  </td>
+                  <td className="w-33">
+                    <div className="px-2 py-[2.5px] w-fit rounded-full bg-(--success-bg) text-(--text-success) text-xs">
+												Terkirim
+                    </div>
+                  </td>
+                  <td className="w-33">
+                    <ul className="flex gap-3 items-center">
+                      <li className=" text-xs">
+                        <button className="cursor-pointer">
+                          <img src={Watch} alt="" />
+                        </button>
+                      </li>
+                      <li className=" text-xs">
+                        <button className="cursor-pointer">
+                          <img src={null} alt="" />
+                        </button>
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>			
+        </header>
+      </div>           
     </DashboardLayout>
   )
 }
